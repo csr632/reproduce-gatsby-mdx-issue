e93f7224f05c8136ea0362724d78c11d1d7958b3
sirui.csr <sirui.csr@alibaba-inc.com>
2019-08-20 23:43:35 +0800

    reproduce issue

---
+++ src/components/Comp2.js
@@ -0,0 +1,3 @@
+export default () => {
+  return 'comp2';
+}
--- src/components/layout.js
+++ src/components/layout.js
@@ -1,52 +1,20 @@
-/**
- * Layout component that queries for data
- * with Gatsby's useStaticQuery component
- *
- * See: https://www.gatsbyjs.org/docs/use-static-query/
- */
-
 import React from "react"
-import PropTypes from "prop-types"
-import { useStaticQuery, graphql } from "gatsby"
+import { graphql } from "gatsby"
+import MDXRenderer from "gatsby-plugin-mdx/mdx-renderer"
 
-import Header from "./header"
-import "./layout.css"
+export default ({
+  data: {
+    mdx: { body },
+  },
+}) => {
+  return <MDXRenderer>{body}</MDXRenderer>
+}
 
-const Layout = ({ children }) => {
-  const data = useStaticQuery(graphql`
-    query SiteTitleQuery {
-      site {
-        siteMetadata {
-          title
-        }
-      }
+export const pageQuery = graphql`
+  query BlogPostQuery($mdxId: String) {
+    mdx(id: { eq: $mdxId }) {
+      id
+      body
     }
-  `)
-
-  return (
-    <>
-      <Header siteTitle={data.site.siteMetadata.title} />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0px 1.0875rem 1.45rem`,
-          paddingTop: 0,
-        }}
-      >
-        <main>{children}</main>
-        <footer>
-          © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.org">Gatsby</a>
-        </footer>
-      </div>
-    </>
-  )
-}
-
-Layout.propTypes = {
-  children: PropTypes.node.isRequired,
-}
-
-export default Layout
+  }
+`
